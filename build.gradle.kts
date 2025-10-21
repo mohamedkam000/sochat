e7e92a264a80a3f26fe862eb10635bf9bb9907f5
Muhammad Kamal <mohamedkam000@gmail.com>
2025-10-21 14:15:30 +0100

    fix gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.13" apply false
-    id("com.android.library") version "8.13" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    id("com.android.application") version "9.0.0-alpha10" apply false
+    id("com.android.library") version "9.0.0-alpha10" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0-Beta1" apply false
+    id("com.google.devtools.ksp") version "1.5.30-1.0.0" apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.serialization) apply false
 }

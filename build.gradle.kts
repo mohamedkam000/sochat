8a7fa28cf48f333514cfa56b23a276bef1309a3a
Muhammad Kamal <mohamedkam000@gmail.com>
2025-10-21 14:19:27 +0100

    fix agp 9 suggestions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application") version "9.0.0-alpha10" apply false
     id("com.android.library") version "9.0.0-alpha10" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0-Beta1" apply false
     id("com.google.devtools.ksp") version "1.5.30-1.0.0" apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.serialization) apply false
